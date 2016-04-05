b886d3598eaf80d14885e11fd838221a40f98024
Igor Vinokur <ivinokur@gmail.com>
2016-04-05 11:17:11 +0000

    master: erte

--- src/main/java/codenvy/client/AppGinInjector.java
+++ src/main/java/codenvy/client/AppGinInjector.java
@@ -5,6 +5,8 @@ import codenvy.client.mainPage.MainPagePresenter;
 import com.google.gwt.inject.client.GinModules;
 import com.google.gwt.inject.client.Ginjector;
 
+//
+
 @GinModules(AppGinModule.class)
 public interface AppGinInjector extends Ginjector {
     MainPagePresenter getMainPagePresenter();
